9e3348494805b4581c4d71182932f8a800d3faf7
PurpleSweetPotatoes <purplesweetpottoes@gmail.com>
2017-07-20 22:51:18 +0800

    add auto cancel task
    add auto cancel task

--- SwiftKit/newWork/BQAPIManager.swift
+++ SwiftKit/newWork/BQAPIManager.swift
@@ -24,14 +24,18 @@ let baseUrl = base_url[sever.rawValue]
 
 class BQAPIManager: NSObject {
     
-    @discardableResult
-    public class func sendRequest(request: BQRequest, completionHandler:@escaping () -> ()) -> URLSessionDataTask? {
-        return BQNetWork.sendRequest(urlstr: baseUrl + request.url(), parameter: request.toDiction(), method: request.method, time: 10, headers: nil) { (data, response, error) in
-            request.responseAction(data: data, response: response, error: error)
+    public class func sendRequest(request: BQRequest, completionHandler:@escaping () -> ()) {
+        weak var req = request
+        let task = BQNetWork.sendRequest(urlstr: baseUrl + request.url(), parameter: request.toDiction(), method: request.method, time: 10, headers: nil) { (data, response, error) in
+            req?.responseAction(data: data, response: response, error: error)
             DispatchQueue.main.async {
-                completionHandler()
+                if req?.task != nil {
+                    completionHandler()
+                    req?.task = nil
+                }
             }
         }
+        request.task = task
     }
 }
 
