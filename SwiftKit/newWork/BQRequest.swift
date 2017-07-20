4733b9b3f9dc496e7c64c9fa38b1c50d2bf336b9
PurpleSweetPotatoes <purplesweetpottoes@gmail.com>
2017-07-20 22:52:17 +0800

    add auto cancel task
    add auto cancel task

--- SwiftKit/newWork/BQRequest.swift
+++ SwiftKit/newWork/BQRequest.swift
@@ -9,35 +9,43 @@
 import UIKit
 
 class BQRequest: NSObject {
-    
     var method: HTTPMethod = .post
-    
-    /// loadResult from network
     var result: Any?
+    weak var task: URLSessionTask?
 
-    /// to set API url,subclass must override
     public func url() -> String { return "" }
-    
     public func toDiction() -> [String: Any] {
         let mir = Mirror(reflecting: self)
         var dict = [String: AnyObject]()
         for p in mir.children {
-            if p.label == "method" || p.label == "result" {
+            if p.label == "method" || p.label == "result" || p.label == "task"{
                 continue
             }
             dict[p.label!] = (p.value as AnyObject)
         }
         return dict
     }
-    
-    
-    /// base action Response, subclass should use this method, then action self handler
     public func responseAction(data: Data?, response: URLResponse?, error: Error?) {
         if let data = data {
-            self.result = try! JSONSerialization.jsonObject(with: data, options: .allowFragments)
+            do {
+                self.result = try JSONSerialization.jsonObject(with: data, options: .allowFragments)
+            } catch let err as NSError {
+                print(err.localizedDescription)
+            }
         }
         if let error = error {
             print(error.localizedDescription)
+        }
+    }
+    deinit {
+        print("request释放")
+        if let task = self.task{
+            if task.state == .running || task.state == .suspended {
+                print("cancel \(task)")
+                task.cancel()
+            }
+            self.task = nil
+            print("释放task")
         }
     }
 }
