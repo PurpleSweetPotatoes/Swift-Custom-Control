f0e409aee8d3561265ae8b398c0a9bf357ba4ffd
MrBai <568604944@qq.com>
2019-09-16 09:24:46 +0800

    update string extension

--- BQKit/extension/String+BQExtension.swift
+++ BQKit/extension/String+BQExtension.swift
@@ -26,7 +26,11 @@ private var Regular_hasChinese = "[\\u4e00-\\u9fa5]"
 extension String {
     
     public func isPhone() -> Bool {
-        return self =~ Regular_Phone
+//        return self =~ Regular_Phone
+        guard let detector = try? NSDataDetector(types: NSTextCheckingResult.CheckingType.phoneNumber.rawValue) else {
+            return false
+        }
+        return detector.numberOfMatches(in: self, range: NSRange(location: 0, length: self.count)) > 0
     }
     
     public func isEmail() -> Bool {
@@ -85,6 +89,32 @@ extension String {
         } catch {
             return nil
         }
+    }
+    
+    /// 将中文字符串转换为拼音
+    ///
+    /// - Parameter hasBlank: 是否带空格（默认不带空格）
+    func transformToPinyin(hasBlank: Bool = false) -> String {
+        
+        let stringRef = NSMutableString(string: self) as CFMutableString
+        CFStringTransform(stringRef,nil, kCFStringTransformToLatin, false) // 转换为带音标的拼音
+        CFStringTransform(stringRef, nil, kCFStringTransformStripCombiningMarks, false) // 去掉音标
+        let pinyin = stringRef as String
+        return hasBlank ? pinyin : pinyin.replacingOccurrences(of: " ", with: "")
+    }
+    
+    /// 获取中文首字母
+    ///
+    /// - Parameter lowercased: 是否小写（默认小写）
+    func transformToPinyinHead(lowercased: Bool = true) -> String {
+        let pinyin = transformToPinyin(hasBlank: true).capitalized // 字符串转换为首字母大写
+        var headPinyinStr = ""
+        for (_, ch) in pinyin.enumerated() {
+            if ch <= "Z" && ch >= "A" {
+                headPinyinStr.append(ch) // 获取所有大写字母
+            }
+        }
+        return lowercased ? headPinyinStr.lowercased() : headPinyinStr
     }
     
     subscript(range: NSRange) -> String {
