675c93e5ba414c3f659bb7459b3f2e4c47e1368b
MrBai <568604944@qq.com>
2022-05-15 22:16:48 +0800

    update

--- classes/extension/AVAssetImageGenerator+BQExtension.swift
+++ classes/extension/AVAssetImageGenerator+BQExtension.swift
@@ -8,6 +8,7 @@
 // *******************************************
 
 import AVFoundation
+import UIKit
 
 public extension AVAssetImageGenerator {
     func frameImage(_ atTime: CMTimeValue) -> UIImage? {
