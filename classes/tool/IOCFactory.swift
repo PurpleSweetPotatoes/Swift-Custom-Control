6c0d20772f2999dd879e619244a612f231e80c5d
MrBai <568604944@qq.com>
2023-05-21 21:18:04 +0800

    add IOC Function

---
+++ classes/tool/IOCFactory.swift
@@ -0,0 +1,27 @@
+//
+//  IOCFactory.swift
+//  BQSwiftKit
+//
+//  Created by baiqiang on 2023/5/21.
+//
+
+import UIKit
+
+public struct IOCFactory {
+
+    private static var instanceMap: [String : Any] = [:]
+
+    public static func register(instance: Any) {
+        let key = "\(type(of: instance))"
+        print("register key: \(key)")
+        instanceMap[key] = instance
+    }
+
+    public static func load<T>() -> T {
+        let key = "\(T.self)"
+        guard let instance = instanceMap[key] as? T else {
+            assert(false, "IOC container don't have \(key)")
+        }
+        return instance
+    }
+}
